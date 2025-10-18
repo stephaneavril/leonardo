c5457d4cc97c41183dc8bc0523efa8cf19d6f3c1
Tu Nombreavril <avril@teams.com.mx>
2025-10-17 19:11:03 -0600

    Corregir ruta relativa de db en API auth (de nuevo)

--- app/api/get-access-token/auth/route.ts
+++ app/api/get-access-token/auth/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { db } from "../../../lib/db";
+import { db } from "../../lib/db";
 // Importa una librería para comparar contraseñas (la instalaremos después)
 // import bcrypt from "bcrypt"; 
 
