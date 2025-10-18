87c3830688397d8c3144dd6125a31ec2a8740e91
Tu Nombreavril <avril@teams.com.mx>
2025-10-17 19:25:10 -0600

    Corregir ruta db (3 puntos) y eliminar route.ts duplicado

--- app/api/get-access-token/auth/[...nextauth]/route.ts
+++ app/api/get-access-token/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { db } from "@/lib/db";
-// // Importa una librería para comparar contraseñas (la instalaremos después)
+import { db } from "../../../lib/db"; // <-- ¡Tres puntos!
+// // // Importa una librería para comparar contraseñas (la instalaremos después)
 // import bcrypt from "bcrypt"; 
 
 const handler = NextAuth({
